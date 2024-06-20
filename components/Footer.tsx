e04dcbba7a02cca4e9b315b72d6bb3c34389e2e9
Ridzwan (Rizz) Nur Saidy <rnursaidy@gmail.com>
2024-06-20 13:24:30 -0700

    re-initalize project

--- components/Footer.tsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-    return (
-      <footer className="bg-white border-t mt-8 py-4 text-center">
-        <p>&copy; 2024 Riz Nur Saidy. All rights reserved.</p>
-      </footer>
-    );
-  }
-  
--- components/Hero.tsx
+++
@@ -1,9 +0,0 @@
-export default function Hero() {
-    return (
-      <section id="home" className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-center">
-        <h1 className="text-6xl font-bold">Riz Nur Saidy</h1>
-        <p className="text-2xl mt-4">Software Developer</p>
-      </section>
-    );
-  }
-  
